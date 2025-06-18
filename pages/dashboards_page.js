ae0442c48563e30f575905dabcb57625efe74f54
khalildaibes <khalildaibes1@gmail.com>
2025-06-19 01:35:01 +0300

    revert dashboards page

--- pages/dashboards_page.js
+++ pages/dashboards_page.js
@@ -1,27 +1,27 @@
-// import React from 'react';
-// import { client } from '../lib/client';
-// import {Dashboard} from '../components';
+import React from 'react';
+import { client } from '../lib/client';
+import {Dashboard} from '../components';
 
 
-// const Dashboards = ({ orders }) => {
+const Dashboards = ({ orders }) => {
 
 
-//   return (
-//     <div >
-//         <Dashboard orders={orders} />
-//     </div>
-//   );
-// };
+  return (
+    <div >
+        <Dashboard orders={orders} />
+    </div>
+  );
+};
 
-// // Fetch data based on the brand query parameter
-// export const getStaticProps = async (context) => {
+// Fetch data based on the brand query parameter
+export const getStaticProps = async (context) => {
 
-//   const ordersQuery = '*[_type == "orders"]'; // Fetch brands from "brand" schema
-//   const orders = await client.fetch(ordersQuery);
+  const ordersQuery = '*[_type == "orders"]'; // Fetch brands from "brand" schema
+  const orders = await client.fetch(ordersQuery);
 
-//   return {
-//     props: { orders }
-//   };
-// };
+  return {
+    props: { orders }
+  };
+};
 
-// export default Dashboards;
+export default Dashboards;
